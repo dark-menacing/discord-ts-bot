4a1f36988dd65723d9554119b4919f01aa4cb2ee
random_developer <darkmenacing@gmail.com>
2023-08-08 07:46:35 -0300

    Updated ping.ts
    Updated its description and run function.

--- src/commands/common/ping.ts
+++ src/commands/common/ping.ts
@@ -4,8 +4,8 @@ import { Command } from "../../structs/types/command";
 export default new Command({
     name: "ping",
     type: ApplicationCommandType.ChatInput,
-    description: "Replies with pong",
-    run({interaction}) {
-        interaction.reply('bing chilling');
+    description: "Replies with your current ping",
+    async run({interaction}) {
+        interaction.reply(`pong! 🏓`);
     }
 })
