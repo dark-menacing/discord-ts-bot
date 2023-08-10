68dafe4f4857084dd81ef7955b1af239ccde9f43
random_developer <darkmenacing@gmail.com>
2023-08-10 18:40:41 -0300

    Created new command
    Created new ban command.

---
+++ src/commands/private/ban.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommandOptionType, ApplicationCommandType, ColorResolvable, EmbedBuilder } from "discord.js";
+import { config } from "../..";
+import { Command } from "../../structs/types/command";
+
+export default new Command({
+    name: "ban",
+    type: ApplicationCommandType.ChatInput,
+    description: "Bans a user",
+    options: [
+        {
+            name: "user",
+            description: "The user you want to ban.",
+            type: ApplicationCommandOptionType.User,
+            required: true
+        },
+        {
+            name: "reason",
+            description: "The reason you want to ban.",
+            type: ApplicationCommandOptionType.String
+        }
+    ],
+    async run({interaction, options}) {
+        if (!interaction.inCachedGuild()) return;
+        const user = options.getUser("user", true);
+        const reason = options.getString("reason") || "No reason provided";
+
+        const {guild} = interaction;
+
+        const {members: memberManager} = guild;
+
+        const embed = new EmbedBuilder()
+        .setTitle('User Banned!')
+        .setDescription(`User ${user} has been banned by ${interaction.user}, by the following reason: ${reason}`)
+        .setColor(config.colors.blue as ColorResolvable)
+        .setAuthor({
+            name: "craig tucker",
+            iconURL: interaction.user.avatarURL() || undefined,
+        })
+        .setTimestamp();
+
+        const DMEmbed = new EmbedBuilder()
+        .setTitle('You have been banned.')
+        .setDescription(`You have been banned by ${interaction.user}, for the following reason: ${reason}`)
+        .setColor(config.colors.red as ColorResolvable)
+        .setAuthor({
+            name: "craig tucker",
+            iconURL: interaction.user.avatarURL() || undefined,
+        })
+        .setTimestamp();;
+
+        (await user.createDM()).send({embeds: [DMEmbed]});
+        await memberManager.ban(user.id, {reason});
+        await interaction.reply({embeds: [embed]});
+    }
+})
